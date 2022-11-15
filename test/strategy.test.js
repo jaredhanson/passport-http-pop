2dc00f78338a2d002ed3729ddd4d4a97990b374f
Jared Hanson <jaredhanson@gmail.com>
2022-11-14 17:27:02 -0800

    Parse JWS.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -12,4 +12,32 @@ describe('Strategy', function() {
     expect(strategy.name).to.equal('pop');
   });
   
+  // https://developers.commerce.campaignmonitor.com/#oauth
+  
+  it('should authenticate request with valid credentials', function(done) {
+    var strategy = new Strategy(function(token, cb) {
+      expect(token).to.equal('123456');
+      return cb(null, { id: '248289761001' }, 'partner-app-client-secret');
+    });
+    
+    // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.Pocf1CzRha25nwCfoZynProYLcV1UE5SlcRGa3qzZXo
+    
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.connection = {};
+        req.connection.encrypted = true;
+        req.url = '/protectedresource';
+        req.headers['host'] = 'resource.example.org';
+        //req.headers['authorization'] = 'PoP eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.J6DUAy9TopLOfIJHFbY2BNDankWID9ZvJ-ylHoV_a6k';
+        req.headers['authorization'] = 'PoP eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.Pocf1CzRha25nwCfoZynProYLcV1UE5SlcRGa3qzZXo';
+      })
+      .success(function(user, info) {
+        expect(user).to.deep.equal({ id: '248289761001' });
+        expect(info).to.be.undefined;
+        done();
+      })
+      .authenticate();
+  });
+  
+  
 });
