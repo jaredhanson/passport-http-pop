c72834522cebaa7c39b5811e3bd7042b421fd97d
Jared Hanson <jaredhanson@gmail.com>
2022-11-16 11:26:09 -0800

    Add tests.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -41,5 +41,33 @@ describe('Strategy', function() {
       .authenticate();
   }); // should authenticate request with valid credentials
   
+  it('should refuse request with signature transmitted in both header field and form-encoded body parameter', function(done) {
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.headers['authorization'] = 'PoP eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.Pocf1CzRha25nwCfoZynProYLcV1UE5SlcRGa3qzZXo';
+        req.body = {};
+        req.body.pop_access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.Pocf1CzRha25nwCfoZynProYLcV1UE5SlcRGa3qzZXo';
+      })
+      .fail(function(status) {
+        expect(status).to.equal(400);
+        done();
+      })
+      .authenticate();
+  });
+  
+  it('should refuse request with signature transmitted in both header field and URI query parameter', function(done) {
+    chai.passport.use(strategy)
+      .request(function(req) {
+        req.headers['authorization'] = 'PoP eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.Pocf1CzRha25nwCfoZynProYLcV1UE5SlcRGa3qzZXo';
+        req.query = {};
+        req.query.pop_access_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdCI6IjEyMzQ1NiIsInRzIjoxMjM0NTY3OH0.Pocf1CzRha25nwCfoZynProYLcV1UE5SlcRGa3qzZXo';
+      })
+      .fail(function(status) {
+        expect(status).to.equal(400);
+        done();
+      })
+      .authenticate();
+  });
+  
   
 });
